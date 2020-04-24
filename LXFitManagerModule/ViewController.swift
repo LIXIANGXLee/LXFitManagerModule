7c14645c41e1de744b26206a2c4ae1358ee2734c
lixiang <lixiang@gokuaidian.com>
2020-04-24 12:25:08 +0800

    Initial Commit

---
+++ LXFitManagerModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LXFitManagerModule
+//
+//  Created by Mac on 2020/4/24.
+//  Copyright © 2020 李响. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
