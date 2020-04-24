ce336b25fdbbeacaf12252455887b976e89df1d9
lixiang <lixiang@gokuaidian.com>
2020-04-24 12:47:03 +0800

    fit

--- LXFitManagerModule/ViewController.swift
+++ LXFitManagerModule/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+               
+        print("------\(LXFit.fitInt(12))")
+        print("------\(UIFont.systemFont(ofSize: 12).fitFont)")
+
     }
 
 
